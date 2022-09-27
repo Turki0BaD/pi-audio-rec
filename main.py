6a47b307bbf0aab178ea10083d658fa280d204cf
Turki0BaD <110273917+Turki0BaD@users.noreply.github.com>
2022-09-27 11:24:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import time
 from datetime import datetime
 import PiRecord
-import PiTimeControl
+
 
 S = datetime.now()
 CT = S.strftime('%H:%M:%S')
 I = '24:01:01'
 x = 0
-
-
+RECschedule = {}
+reclength = {}
 while S != I:
+    RECschedule = {}
+    with open("RECTIME.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            RECschedule[str(key)] = int(val)
+    reclength = RECschedule
     S = datetime.now()
     CT = S.strftime('%H:%M:%S')
     time.sleep(1)
-    if CT in PiTimeControl.reclength:
-        rt = PiTimeControl.reclength[CT]
+    if CT in reclength:
+        rt = reclength[CT]
         PiRecord.recorde(rt)
