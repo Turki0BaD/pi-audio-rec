41693bb673119c6e3d32c6946cfa1d3b6747d226
Turki0BaD <110273917+Turki0BaD@users.noreply.github.com>
2022-09-29 09:35:15 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-from os import listdir
 
 app = Flask(__name__)
 
@@ -8,34 +7,35 @@ app = Flask(__name__)
 def my_form():
     return render_template('index.html')
 
-@app.route('/AddTime', methods=['GET','POST'])
+
+@app.route('/AddTime', methods=['GET', 'POST'])
 def Add_time():
     rec_dur = request.form.get('recdur')
     rec_ti = request.form.get('rectime')
     if request.method == 'POST':
-        with open("RECTIME.txt", 'a') as Fa: 
-            Fa.write(str(rec_ti + " " + rec_dur +"\n"))
+        with open("RECTIME.txt", 'a') as Fa:
+            Fa.write(str("\n" + rec_ti + " " + rec_dur))
     return render_template('AddTime.html')
 
-@app.route('/Remove', methods=['GET','POST'])
+
+@app.route('/Remove', methods=['GET', 'POST'])
 def Remove():
     rem_dur = request.form.get('remdur')
     rem_ti = request.form.get('remtime')
     if request.method == 'POST':
-        with open('RECTIME.txt', 'r') as file :
+        with open('RECTIME.txt', 'r') as file:
             filedata = file.read()
-            filedata = filedata.replace(str(rem_ti+" "+rem_dur+"\n"), '')
+            filedata = filedata.replace(str(rem_ti + " " + rem_dur), '')
         with open('RECTIME.txt', 'w') as file:
             file.write(filedata)
     return render_template('Remove.html')
 
-@app.route('/Preview') 
-def content(): 
-	with open('RECTIME.txt', 'r') as f: 
-		return render_template('Preview.html', text=f.read())
 
+@app.route('/Preview')
+def content():
+    with open('RECTIME.txt', 'r') as f:
+        return render_template('Preview.html', text=f.read())
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
